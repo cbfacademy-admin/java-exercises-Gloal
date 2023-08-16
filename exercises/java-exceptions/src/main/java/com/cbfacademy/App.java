09639e1ad3daeba3065886ebdcc70b25fe1119d1
Alori G <alori.glo@gmail.com>
2023-08-16 14:49:28 +0100

    java exceptions exercise

--- exercises/java-exceptions/src/main/java/com/cbfacademy/App.java
+++ exercises/java-exceptions/src/main/java/com/cbfacademy/App.java
@@ -1,13 +1,39 @@
 package com.cbfacademy;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+import org.powermock.reflect.exceptions.FieldNotFoundException;
+
+import it.larusba.neo4j.jdbc.impl.ListArray;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws NullFilenameException
     {
-        System.out.println( "Hello World!" );
+        HashMap<String, Integer> filesMap = new HashMap<String, Integer>();
+      
+        String filesArray[] = new String[]{"App.java", "App.txt", null, "App.md"};
+
+        List<String> filenames = new ArrayList<String>(Arrays.asList(filesArray));
+
+        for (String s:filenames){
+            FileExtension fe = new FileExtension();
+            int key = 0;
+            try{
+                key = fe.check(s);
+            }catch (NullFilenameException e){
+                key = -1;
+            }finally{
+            filesMap.put(s, key);
+            }
+        }
+        System.out.println(filesMap);
     }
 }
